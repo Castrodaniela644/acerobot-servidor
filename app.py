3d5b7b9add26eaa90eebf6188f296dd06fa91c60
Castrodaniela644 <castrodaniela644@gmail.com>
2025-07-16 23:10:21 -0500

    Update app.py

--- app.py
+++ app.py
@@ -26,4 +26,7 @@ def webhook():
     return {"message": reply}
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    puerto = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host="0.0.0.0", port=puerto)
+
+
